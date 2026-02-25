e17d3052397724120346be0b89f621cda47518f2
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-02-25 09:52:52 +0100

    feat: criação da taks fetch_data , mock_fetch_data e connect_to_db.
    fetch_data é para ingerir os dados da API e connect_to_db é para conectar ao banco de dados postgresql que está rodando no container docker. Além disso existe a função mock_fetch_data que simula uma resposta da API. Isso foi feito pois a API tem limite de requisições no plano gratuito.

---
+++ tasks.py
@@ -0,0 +1,47 @@
+import os
+
+import psycopg2
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PG_HOST = os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_DB   = os.getenv("PG_DB")
+PG_USER = os.getenv("PG_USER")
+PG_PASS = os.getenv("PG_PASS")
+
+api_url = "https://api.weatherstack.com/current?access_key=882ce2116a49dfa95b87cc876d840c8c&query=Brazil"
+
+# Função para extração dos dados da API.
+def fetch_data():
+    print("Tentando conectar à API")
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+
+        print("Conexão com a api feita com sucesso!")
+        return response.json()
+    except requests.exceptions.RequestsException as e:
+        print(f"Ocorreu um erro {e}")
+        raise
+
+def mock_fetch_data():
+    return "{'request': {'type': 'City', 'query': 'Brasilia, Brazil', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'Brasilia', 'country': 'Brazil', 'region': 'Distrito Federal', 'lat': '-15.783', 'lon': '-47.917', 'timezone_id': 'America/Sao_Paulo', 'localtime': '2026-02-25 05:37', 'localtime_epoch': 1771997820, 'utc_offset': '-3.0'}, 'current': {'observation_time': '08:37 AM', 'temperature': 20, 'weather_code': 116, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'], 'weather_descriptions': ['Partly cloudy'], 'astro': {'sunrise': '06:11 AM', 'sunset': '06:38 PM', 'moonrise': '01:52 PM', 'moonset': '12:07 AM', 'moon_phase': 'Waxing Gibbous', 'moon_illumination': 55}, 'air_quality': {'co': '184.85', 'no2': '3.45', 'o3': '28', 'so2': '1.25', 'pm2_5': '2.95', 'pm10': '3.05', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 10, 'wind_degree': 301, 'wind_dir': 'WNW', 'pressure': 1014, 'precip': 0, 'humidity': 94, 'cloudcover': 75, 'feelslike': 20, 'uv_index': 0, 'visibility': 10, 'is_day': 'no'}}"
+
+def connect_to_db():
+    print("Conectando ao banco de dados PostgreSQL")
+    try:
+        conn = psycopg2.connect(
+            host = PG_HOST,
+            port = PG_PORT,
+            dbname = PG_DB,
+            user = PG_USER,
+            password = PG_PASS
+        )
+        return conn
+    except psycopg2.Error as e:
+        print(f"A conexão com o banco de dados PostgreSQL falhou: {e}")
+        raise
+connect_to_db()
