f2064673042127f4fc03d91899086d234d75bc62
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-02-25 10:47:10 +0100

    chore: Foi adicionado uma nova funcionalidade com a função insert_records, além de refatoração nas funções create_table e mock_fetch_data.
    insert records é para inserir os registros extraídos da api no banco de dados postgresql que está rodando no docker.
    
    na função create_table fiz uma alteração para garantir que o banco de dados seja fechado.
    
    na função mock_fetch_data tirei as aspas que estavam fazendo com que o dicionário virasse string.

--- tasks.py
+++ tasks.py
@@ -29,7 +29,7 @@ def fetch_data():
 
 # Função para simular os dados retornados na resposta da API.
 def mock_fetch_data():
-    return "{'request': {'type': 'City', 'query': 'Brasilia, Brazil', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'Brasilia', 'country': 'Brazil', 'region': 'Distrito Federal', 'lat': '-15.783', 'lon': '-47.917', 'timezone_id': 'America/Sao_Paulo', 'localtime': '2026-02-25 05:37', 'localtime_epoch': 1771997820, 'utc_offset': '-3.0'}, 'current': {'observation_time': '08:37 AM', 'temperature': 20, 'weather_code': 116, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'], 'weather_descriptions': ['Partly cloudy'], 'astro': {'sunrise': '06:11 AM', 'sunset': '06:38 PM', 'moonrise': '01:52 PM', 'moonset': '12:07 AM', 'moon_phase': 'Waxing Gibbous', 'moon_illumination': 55}, 'air_quality': {'co': '184.85', 'no2': '3.45', 'o3': '28', 'so2': '1.25', 'pm2_5': '2.95', 'pm10': '3.05', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 10, 'wind_degree': 301, 'wind_dir': 'WNW', 'pressure': 1014, 'precip': 0, 'humidity': 94, 'cloudcover': 75, 'feelslike': 20, 'uv_index': 0, 'visibility': 10, 'is_day': 'no'}}"
+    return {'request': {'type': 'City', 'query': 'Brasilia, Brazil', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'Brasilia', 'country': 'Brazil', 'region': 'Distrito Federal', 'lat': '-15.783', 'lon': '-47.917', 'timezone_id': 'America/Sao_Paulo', 'localtime': '2026-02-25 05:37', 'localtime_epoch': 1771997820, 'utc_offset': '-3.0'}, 'current': {'observation_time': '08:37 AM', 'temperature': 20, 'weather_code': 116, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'], 'weather_descriptions': ['Partly cloudy'], 'astro': {'sunrise': '06:11 AM', 'sunset': '06:38 PM', 'moonrise': '01:52 PM', 'moonset': '12:07 AM', 'moon_phase': 'Waxing Gibbous', 'moon_illumination': 55}, 'air_quality': {'co': '184.85', 'no2': '3.45', 'o3': '28', 'so2': '1.25', 'pm2_5': '2.95', 'pm10': '3.05', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 10, 'wind_degree': 301, 'wind_dir': 'WNW', 'pressure': 1014, 'precip': 0, 'humidity': 94, 'cloudcover': 75, 'feelslike': 20, 'uv_index': 0, 'visibility': 10, 'is_day': 'no'}}
 
 #Função para conectar ao banco de dados que está sendo executado no container docker.
 def connect_to_db():
@@ -47,6 +47,7 @@ def connect_to_db():
         print(f"A conexão com o banco de dados PostgreSQL falhou: {e}")
         raise
 
+#Função responsável por criar a tabela e schema caso não existam.
 def create_table(conn):
     print("Criando a tabela raw_clima")
     try:
@@ -65,10 +66,42 @@ def create_table(conn):
             )
         """)
         conn.commit()
+        conn.close()
         print("Tabela criada com sucesso!")
     except psycopg2.Error as e:
         print(f"Erro ao criar a tabela: {e}")
         raise
 
+def insert_records(conn, data):
+    try:
+        weather = data['current']
+        location = data['location']
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO dev.raw_clima(
+                cidade,
+                temperatura,
+                clima_descricao,
+                vento_velocidade,
+                horario,
+                insert_at,
+                utc_diferenca
+            ) VALUES (%s, %s, %s, %s, %s, NOW(), %s)
+        """, (
+            location['name'],
+            weather['temperature'],
+            weather['weather_descriptions'][0],
+            weather['wind_speed'],
+            location['localtime'],
+            location['utc_offset']
+        ))
+        conn.commit()
+        conn.close()
+        print("Dados inseridos na tabela dev.raw_clima com sucesso!")
+    except psycopg2.Error as e:
+        print(f"Erro durante a inserção dos dados na tabela dev.raw_clima: {e}")
+        raise
+
+data = mock_fetch_data()
 conn = connect_to_db()
-create_table(conn)
+insert_records(conn, data)
