88a086b1bddf5890a0686c55b6a9ff3cfe5b9564
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-02-25 10:12:10 +0100

    feat: Criação da função create_table.
    Esta função serve para criar o schema e a tabela responsável por armazenar os dados raw extraídos da API.
    
    Resolvi colocar o SQL com a descrição da tabela na função mesmo por motivos de praticidade e também por ser tratar de um projeto com dados públicos e não sensíveis, mas caso fosse um projeto com dados sensíveis de alguma empresa privada seria ideal escrever o código de criação de schema e tabela em um arquivo .sql e não fazer o commit desse arquivo.

--- tasks.py
+++ tasks.py
@@ -27,9 +27,11 @@ def fetch_data():
         print(f"Ocorreu um erro {e}")
         raise
 
+# Função para simular os dados retornados na resposta da API.
 def mock_fetch_data():
     return "{'request': {'type': 'City', 'query': 'Brasilia, Brazil', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'Brasilia', 'country': 'Brazil', 'region': 'Distrito Federal', 'lat': '-15.783', 'lon': '-47.917', 'timezone_id': 'America/Sao_Paulo', 'localtime': '2026-02-25 05:37', 'localtime_epoch': 1771997820, 'utc_offset': '-3.0'}, 'current': {'observation_time': '08:37 AM', 'temperature': 20, 'weather_code': 116, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png'], 'weather_descriptions': ['Partly cloudy'], 'astro': {'sunrise': '06:11 AM', 'sunset': '06:38 PM', 'moonrise': '01:52 PM', 'moonset': '12:07 AM', 'moon_phase': 'Waxing Gibbous', 'moon_illumination': 55}, 'air_quality': {'co': '184.85', 'no2': '3.45', 'o3': '28', 'so2': '1.25', 'pm2_5': '2.95', 'pm10': '3.05', 'us-epa-index': '1', 'gb-defra-index': '1'}, 'wind_speed': 10, 'wind_degree': 301, 'wind_dir': 'WNW', 'pressure': 1014, 'precip': 0, 'humidity': 94, 'cloudcover': 75, 'feelslike': 20, 'uv_index': 0, 'visibility': 10, 'is_day': 'no'}}"
 
+#Função para conectar ao banco de dados que está sendo executado no container docker.
 def connect_to_db():
     print("Conectando ao banco de dados PostgreSQL")
     try:
@@ -44,4 +46,29 @@ def connect_to_db():
     except psycopg2.Error as e:
         print(f"A conexão com o banco de dados PostgreSQL falhou: {e}")
         raise
-connect_to_db()
+
+def create_table(conn):
+    print("Criando a tabela raw_clima")
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE SCHEMA IF NOT EXISTS dev;
+            CREATE TABLE IF NOT EXISTS dev.raw_clima(
+                id SERIAL PRIMARY KEY,
+                cidade TEXT,
+                temperatura FLOAT,
+                clima_descricao TEXT,
+                vento_velocidade FLOAT,
+                horario TIMESTAMP,
+                insert_at TIMESTAMP DEFAULT NOW(),
+                utc_diferenca TEXT
+            )
+        """)
+        conn.commit()
+        print("Tabela criada com sucesso!")
+    except psycopg2.Error as e:
+        print(f"Erro ao criar a tabela: {e}")
+        raise
+
+conn = connect_to_db()
+create_table(conn)
