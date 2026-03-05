7918c1967d37455eec3fd6e6c467be599dc98c27
Victor Rodrigues <contactvictorcrespo@gmail.com>
2026-03-05 14:52:49 +0100

    refactor: garantindo que o diretório responsável por receber os arquivos tratados existe.

--- dags/utils/tasks.py
+++ dags/utils/tasks.py
@@ -1,15 +1,17 @@
 import pandas as pd
-
+import os
 
 def transformar_colunas():
+    os.makedirs("/opt/airflow/tmp", exist_ok=True)
     # ler arquivo
     df = pd.read_excel("/opt/airflow/file.xltx")
 
 
     # renomear colunas
     df = df.rename(columns={
-        "nome": "name",
-        "idade": "age"
+        "Nome": "name",
+        "Idade": "age"
     })
     # salvar resultado
+    df.to_csv("/opt/airflow/tmp/df_example.csv", index=False)
     return df
